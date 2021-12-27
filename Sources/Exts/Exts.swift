2d28863603c3ab074a94c378307e86ce59f7e1e7
Louis <louis@spoonradio.co>
2021-12-28 02:15:07 +0900

    ch

--- Sources/Exts/Exts.swift
+++ Sources/Exts/Exts.swift
@@ -1,5 +1,5 @@
-public struct Exts {
-    public private(set) var text = "Hello, World!"
-
-    public init() { }
-}
+//public struct Exts {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() { }
+//}
--- Sources/Exts/MVVM/VMTableVIewCell.swift
+++ Sources/Exts/MVVM/VMTableVIewCell.swift
@@ -4,8 +4,8 @@ import UIKit
 import RxSwift
 
 open class VMTableViewCell<VM: AnyObject>: UITableViewCell {
-    var bag = DisposeBag()
-    var vm: VM? {
+    public var bag = DisposeBag()
+    public var vm: VM? {
         didSet {
             self.bag = DisposeBag()
             if let vm = self.vm {
